28d6479f2700902a9ca61166dc955e0dfd74cab3
nashid <nashid4722@gmail.com>
2023-04-25 15:48:57 +0300

    edited the prompt

--- src/app.ts
+++ src/app.ts
@@ -53,20 +53,29 @@ console.log('here is the request body');
         {
             "role": "user",
             "content": `The Sustainability Awareness Framework (SusAF) is a tool for sustainable design of software products and services. 
-The SusAF workbook enables you to perform a guided capture analysis of the sustainability of your software. 
-There are 5 dimensions in the SusAF which are social, individual, environmental, economic and technical. 
-There are also 3 orders of effects which are immediate (first order), enabling (second order), and structural (third order). 
-I want you to do a Sustainability Awareness Framework (SusAF) analysis based on the information given here and give me the response in a JSON format. 
-The informations are; 
-Company name: ${company_name } .
-Company goals: ${company_goals } .
-Company mission: ${company_mission} .
-Name of product: ${product_name} and more product ${product_description} and
-Product features: ${product_features} .
-Some SusAF Questions and answers are ${qna}
-For more information.
-You have to provide me with the list of positive and negative impacts in precise words for each of the features in each of the SusAF dimensions mentioned with order of effects. 
-As well as threats and opportunities and possible actions can be taken for the future.`
+            The SusAF workbook enables you to perform a guided capture analysis of the sustainability of your software. 
+            There are 5 dimensions in the SusAF which are social, individual, environmental, economic and technical. 
+            There are also 3 orders of effects which are immediate (first order), enabling (second order), and structural (third order). 
+            Definition of the order effects (Direct, Enabling, Structural): Direct: Immediate are direct effects of the production, operation, use and disposal of socio-technical systems. 
+            This includes the properties and the full lifecycle impacts, such as in the Life-Cycle Assessment (LCA) approach. 
+            Enabling: Enabling of operation and use of a system include any change enabled or induced by the system. Structural: Structural represent structural changes caused by the ongoing operation and use of the socio-technical system. 
+            Definition of Sustainability Dimensions (Social, Environmental, Economic, Technical): Social: covers the relationships between individuals and groups. Individual: covers the individual's ability to thrive, exercise their rights, and develop freely. 
+            Environmental: covers the use and stewardship of natural resources. Economic: covers the financial aspects and business value. Technical: covers the technical system’s ability to accommodate changes. 
+            I want you to do a Sustainability Awareness Framework (SusAF) analysis based on the information given here and give me the response in a JSON format. 
+            The informations are; 
+            Company name: ${company_name } .
+            Company goals: ${company_goals } .
+            Company mission: ${company_mission} .
+            Name of product: ${product_name} and more product ${product_description} and
+            Product features: ${product_features} .
+            Some SusAF Questions and answers are ${qna}
+            For more information.
+            You have to provide me:
+            Incremental count “id” (index of the effect starting from 1);
+            The list of positive and negative impacts in precise words for each of the features as “effect”;
+            Order of effects (there might be multiple), as “order”;
+            Dimension (there might be multiple), as “dimension”;
+            As well as threats and opportunities and possible actions can be taken for the future.`
         }
     ]
     
