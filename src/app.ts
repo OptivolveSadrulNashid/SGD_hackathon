609774d2f4f46e5c39b1357dd759e76b5536f854
Robel Amare Gebrewold <robel@Robels-MacBook-Air.local>
2023-04-25 11:43:13 +0300

    added the request body and got response from chatgpt

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { Pool } from "pg";
 import axios, { AxiosRequestConfig } from 'axios';
 
 const app = express();
+app.use(express.json()); //Used to parse JSON bodies
 dotenv.config(); //Reads .env file and makes it accessible via process.env
 const pool = new Pool({
   host: process.env.DB_HOST,
@@ -35,6 +36,10 @@ app.get("/hi", (req: Request, res: Response, next: NextFunction) => {
 
 
 //=================================================
+interface Payload {
+  data: string;
+}
+
 async function openAiPostRequest(endpoint: string, requestData: any): Promise<any> {
   const config: AxiosRequestConfig = {
     headers: {
@@ -54,22 +59,26 @@ async function openAiPostRequest(endpoint: string, requestData: any): Promise<an
   }
 }
 
-app.post("/example", async (req, res) => {
+app.post("/example", async (req: Request, res: Response, next: NextFunction) => {
+const {data} = req.body;
+const payload: Payload = {data};
+console.log('here is the request body');
+ console.log(data); 
+  // Output the request body
   const requestData1 = {
     "model": "gpt-3.5-turbo",
-    "temperature": 0.5,
+    "temperature": 0,
     "top_p": 1,
     "frequency_penalty": 0,
     "presence_penalty": 0,
     "messages": [
         {
             "role": "user",
-            "content": "do you know about susaf?"
+            "content": `${data}`
         }
     ]
     
 };
-
 
 const response = await openAiPostRequest('completions', requestData1);
 console.log(response); // Output the response from the OpenAI API
